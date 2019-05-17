867a65a6b219490300b32070a86c2f306c4c0688
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-17 22:04:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,16 +21,21 @@ require 'pry'
 def second_supply_for_fourth_of_july(holiday_hash)
   holiday_hash.each {|key, value|
     if key == :summer
-      return value[1]
+      return value[:fourth_of_july][1]
     end
   }
 end
 
-def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
+# holiday_hash is identical to the one above
+# add the second argument, which is a supply, to BOTH the
+# Christmas AND the New Year's arrays
 
+def add_supply_to_winter_holidays(holiday_hash, supply)
+  holiday_hash.each {|key, value|
+    if key == :winter
+      value.push(supply)
+    end
+  }
 end
 
 
